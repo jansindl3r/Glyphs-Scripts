c039cad691e4de6bd4e02915d8819f22115ed1a2
Jan Sindler <jansindl3r@gmail.com>
2024-08-26 22:08:16 +0200

    appendix

--- export/exportWithGlyphsLib.py
+++ export/exportWithGlyphsLib.py
@@ -8,6 +8,7 @@ Bakes smart stuff and exports the current font to UFO, OTF and TTF using glyphsL
 
 import subprocess
 import vanilla
+import re
 from vanilla import dialogs
 
 from GlyphsApp import GSScriptingHandler
@@ -17,6 +18,7 @@ try:
 	import glyphsLib
 	import ufo2ft
 	import fontmake
+	import ufoLib2
  
 except ModuleNotFoundError as e:
 	python_path = GSScriptingHandler.sharedHandler().currentPythonPath()
@@ -37,13 +39,42 @@ from tempfile import NamedTemporaryFile
 from fontmake.instantiator import Instantiator
 from fontTools.designspaceLib import DesignSpaceDocument
 from pathlib import Path
+from ufoLib2 import Font
 
+
+def rename_bracket(glyph_name, appendix):
+    match = re.match(r"(?P<glyph_name>.*)\.BRACKET\.varAlt(?P<var_alt>\d+)", glyph_name)
+    if match:
+        group_dict = match.groupdict()
+        new_name = f"{group_dict['glyph_name']}.{appendix}.{group_dict['var_alt']}"
+        return new_name
+
+def rename_doc(doc, appendix):
+    for rule in doc.rules:
+        for s, (from_sub, to_sub) in enumerate(rule.subs):
+            new_to_sub = rename_bracket(to_sub, appendix)
+            if new_to_sub:
+                rule.subs[s] = (from_sub, new_to_sub)
+
+    doc.write(doc.path)
+
+def rename_font(font, appendix):
+
+	glyphs_to_rename = []
+	for glyph in font:
+		new_glyph_name = rename_bracket(glyph.name, appendix)
+		if new_glyph_name:
+			glyphs_to_rename.append((glyph.name, new_glyph_name))
+
+	for old_name, new_name in glyphs_to_rename:
+		font.renameGlyph(old_name, new_name)
+	font.save()
 
 	
 class Dialog:
 	def __init__(self):
 		self.window = vanilla.FloatingWindow(
-			(300, 175),
+			(300, 205),
 			"Export to UFO with GlyphsLib", 
 		)
 		self.component_elements = []
@@ -51,7 +82,9 @@ class Dialog:
 		self.window.export_otf = vanilla.CheckBox((15, 45, -15, 20), "Export OTF", sizeStyle="small", value=True, callback=self.toggle_export)
 		self.window.export_ttf = vanilla.CheckBox((15, 75, -15, 20), "Export TTF", sizeStyle="small", value=True, callback=self.toggle_export)
 		self.window.export_variable = vanilla.CheckBox((15, 105, -15, 20), "Export Variable", sizeStyle="small", value=True, callback=self.toggle_export)
-		self.window.export_button = vanilla.Button((15, 135, -15, 20), "Export", sizeStyle="small", callback=self.export)
+		self.window.bracket_layer_glyph_appendix_label = vanilla.TextBox((15, 138, -15, 20), "Bracket Layer Appendix", sizeStyle="small")
+		self.window.bracket_layer_glyph_appendix = vanilla.EditText((150, 135, -15, 20), text="rvrn", sizeStyle="small")
+		self.window.export_button = vanilla.Button((15, 165, -15, 20), "Export", sizeStyle="small", callback=self.export)
 		self.window.export_button.enable(False)
 		self.window.open()
 		self.export_folder = None
@@ -100,8 +133,14 @@ class Dialog:
 
 		with NamedTemporaryFile(suffix='.glyphs', delete=True) as temp_file:
 			font.save(temp_file.name)
-			_, designspace_path = build_masters(temp_file.name, self.export_folder/"ufo", minimal=True)
+			ufo_paths, designspace_path = build_masters(temp_file.name, self.export_folder/"ufo", minimal=True)
 			designspace = DesignSpaceDocument.fromfile(designspace_path)
+			bracket_layer_appendix = self.window.bracket_layer_glyph_appendix.get()
+			rename_doc(designspace, bracket_layer_appendix)
+			for ufo_path in ufo_paths:
+				ufo = Font.open(self.export_folder/"ufo"/ufo_path)
+				rename_font(ufo, bracket_layer_appendix)
+				ufo.save()
 			instantiator = Instantiator.from_designspace(designspace)
 			for instance in designspace.instances:
 				ufo = instantiator.generate_instance(instance)
